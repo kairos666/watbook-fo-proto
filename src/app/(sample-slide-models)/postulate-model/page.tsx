51999c4b15098d577e15d01211300ec249cc3385
David Maggi <musashi.miyamoto81@gmail.com>
2023-06-04 09:37:38 +0200

    PostulateModel - interactivity & props

--- src/app/(sample-slide-models)/postulate-model/page.tsx
+++ src/app/(sample-slide-models)/postulate-model/page.tsx
@@ -8,7 +8,20 @@ export const metadata:Metadata = {
 export default function PostulateSample() {
     return (
         <main>
-            <PostulateModel />
+            <PostulateModel
+                slideTitle="Résumons ensemble..."
+                slideIllu="/samples/top-view-pool.png"
+                projectTitle="Projet de Mr Dubriand"
+                checks={[
+                    <>Modèle <b>Barbara</b>. Taille : 7m à 7,90m x 3m à 3,90m</>,
+                    <>En 72 mensualités de confort : <b>694 €</b></>,
+                    <>Modèle <b>Orbis.</b> Coloris : blanc</>,
+                    <>Date du 1er plongeon <b>20 avril 2024</b></>,
+                    <>Ligne d'équipement : <b>Top</b></>,
+                    <>Date démarrage des travaux : <b>20 août 2024</b></>,
+                    <>Budget de <b>50 000 €</b></>
+                ]}
+            />
         </main>
     )
 }
--- src/slide-components/PostulateModel.tsx
+++ src/slide-components/PostulateModel.tsx
@@ -1,33 +1,55 @@
 'use client'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useRef, useState } from 'react';
+import Form from 'react-bootstrap/Form';
 import styles from "./PostulateModel.module.scss";
 
-type PostulateModelProps = {}
+type PostulateModelProps = {
+    slideTitle: string
+    slideIllu: string
+    projectTitle: string
+    checks: React.ReactNode[]
+    customNextLabel?: string
+    nextCb?: Function
+}
 
-export default function PostulateModel({}:PostulateModelProps) {
+export default function PostulateModel({ slideTitle, slideIllu, projectTitle, checks, customNextLabel, nextCb }:PostulateModelProps) {
+    const formRef = useRef(null);
+    const [isFormValid, setIsFormValid] = useState(false);
+    const handleChange = () => {
+        if(formRef === null) return;
+
+        setIsFormValid(((formRef as any).current as HTMLFormElement).checkValidity());
+    }
+    const handleSubmit = (evt:any) => {
+        evt.preventDefault();
+        if(nextCb) {
+            nextCb();
+        } else {
+            alert("slide suivant - bouchonné");
+        }
+    }
+
     return (
-        <article className={ styles["posm-SlideWrapper"] }>
-            <header className={ styles["posm-Header"] }>
-                <button className={ styles["posm-Header-BackBtn"] } onClick={ () => { history.back() } }><FontAwesomeIcon color="#ffffff" icon="arrow-left" size="xl" /></button>
-                <h1 className={ styles["posm-Header-Title"] }>Résumons ensemble...</h1>
-                <button className={ styles["posm-Header-NextBtn"] } onClick={ () => { alert("bouchonné") } }>Suivant</button>
-            </header>
-            <figure className={ styles["posm-Content"] }>
-                <img className={ styles["posm-Content-SlideIllustration"] } alt="slide main illustration" src="/samples/top-view-pool.png" />
-                <figcaption className={ styles["posm-Content-DataContainer"] }>
-                    <h2 className={ styles["posm-Content-SubTitle"] }>Projet de Mr Dubriand</h2>
-                    <ul className={ styles["posm-Content-CheckList"] }>
-                        <li><FontAwesomeIcon color="#007AC8" icon="check" size="2xl" />Modèle <b>Barbara</b>. Taille : 7m à 7,90m x 3m à 3,90m</li>
-                        <li><FontAwesomeIcon color="#007AC8" icon="check" size="2xl" />En 72 mensualités de confort : <b>694 €</b></li>
-                        <li><FontAwesomeIcon color="#007AC8" icon="check" size="2xl" />Modèle <b>Orbis.</b> Coloris : blanc</li>
-                        <li><FontAwesomeIcon color="#007AC8" icon="check" size="2xl" />Date du 1er plongeon <b>20 avril 2024</b></li>
-                        <li><FontAwesomeIcon color="#007AC8" icon="check" size="2xl" />Ligne d'équipement : <b>Top</b></li>
-                        <li><FontAwesomeIcon color="#007AC8" icon="check" size="2xl" />Date démarrage des travaux : <b>20 août 2024</b></li>
-                        <li><FontAwesomeIcon color="#007AC8" icon="check" size="2xl" />Budget de <b>50 000 €</b></li>
-                    </ul>
-                </figcaption>
-            </figure>
-        </article>
+        <Form ref={ formRef } onSubmit={ handleSubmit }>
+            <article className={ styles["posm-SlideWrapper"] }>
+                <header className={ styles["posm-Header"] }>
+                    <button className={ styles["posm-Header-BackBtn"] } onClick={ () => { history.back() } }><FontAwesomeIcon color="#ffffff" icon="arrow-left" size="xl" /></button>
+                    <h1 className={ styles["posm-Header-Title"] }>{ slideTitle }</h1>
+                    <button type="submit" disabled={ !isFormValid } className={ styles["posm-Header-NextBtn"] }>{ (customNextLabel) ? customNextLabel : 'Suivant' }</button>
+                </header>
+                <figure className={ styles["posm-Content"] }>
+                    <img className={ styles["posm-Content-SlideIllustration"] } alt="slide main illustration" src={ slideIllu } />
+                    <figcaption className={ styles["posm-Content-DataContainer"] }>
+                        <h2 className={ styles["posm-Content-SubTitle"] }>{ projectTitle }</h2>
+                        
+                            <ul className={ styles["posm-Content-CheckList"] }>
+                                { checks.map((check, index) => <li key={ index }><Form.Check type="switch" id={ `check-${ index }` } label={ check } required onChange={ handleChange }/></li>) }
+                            </ul>
+                    </figcaption>
+                </figure>
+            </article>
+        </Form>
     );
 }
