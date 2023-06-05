b07ee843dfe03fd147b6e10ad3034caa2edac681
David Maggi <musashi.miyamoto81@gmail.com>
2023-06-05 23:27:01 +0200

    GridSlide

--- src/app/(sample-slide-models)/grid-model/page.tsx
+++ src/app/(sample-slide-models)/grid-model/page.tsx
@@ -1,23 +1,35 @@
-import GridModel, { GridCard } from "@/slide-components/GridModel";
+'use client';
+
+import PoolBuilder from "@/mock-data/PoolBuilder";
+import GridSlide from "@/slide-components/grid/GridSlide";
 import { Metadata } from "next";
 
 export const metadata:Metadata = {
     title: 'modèle : grille de produits'
 }
 
-export default function FreeContentSample() {
+export default function GridSample() {
+    const mockPools = new Array(12)
+        .fill(null)
+        .map(() => PoolBuilder())
+        .map(pool => ({
+            title: pool.libelle,
+            vignette: pool.illustrations[0].src,
+            // initialQt: 1,
+            // qtEnabled: true
+        }));
+
+    const props = {
+        slideTitle: "Choix de la forme et modèle",
+        mandatoryChoice: true,
+        singularChoice: true,
+        hasCart: false,
+        choices: mockPools
+    }
+
     return (
         <main>
-            <GridModel slideTitle="Choix de la forme et modèle">
-                <GridCard title="product 1" vignette="https://picsum.photos/600" selectionType="mono-select" detail="none" />
-                <GridCard title="product 2" vignette="https://picsum.photos/600" selectionType="mono-select" detail="carousel" />
-                <GridCard title="product 3" vignette="https://picsum.photos/600" selectionType="mono-select" detail="none" />
-                <GridCard title="product 4" vignette="https://picsum.photos/600" selectionType="mono-select" detail="free-content" />
-                <GridCard title="Un nom de produit vraiment très long pour voir ce que ça donne" vignette="https://picsum.photos/600" selectionType="mono-select" detail="none" />
-                <GridCard title="product 6" vignette="https://picsum.photos/600" selectionType="mono-select" detail="none" />
-                <GridCard title="product 7" vignette="https://picsum.photos/600" selectionType="mono-select" detail="none" />
-                <GridCard title="product 8" vignette="https://picsum.photos/600" selectionType="mono-select" detail="none" />
-            </GridModel>
+            <GridSlide {...props} />
         </main>
     )
 }
