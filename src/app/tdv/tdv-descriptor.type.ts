26e7031bd6a872d8a0f0468f95c560157e90b7c0
David Maggi <musashi.miyamoto81@gmail.com>
2023-07-02 09:45:05 +0200

    tdv descriptor JSON

---
+++ src/app/tdv/tdv-descriptor.type.ts
@@ -0,0 +1,23 @@
+/**
+ * TDVDescriptor MUST BE PARSABLE TO JSON
+ * When multiple transitions are available, meeting conditions should provide a clear winner (not allowed to have multiple valid transitions at the same time)
+ */
+
+export type TDVStep = {
+    id: string
+    name: string
+    isStartStep?: boolean // is root (one and only one on tree)
+}
+
+export type TDVTransition = {
+    from: string
+    to: string
+    conditions?: string[] // only if conditions have to be met to activate this transition
+    conditionType?: "AND"|"ANY" // necessary if multiple conditions are provided
+}
+
+export type TDVDescriptor = {
+    tdvType: string
+    tdvSteps: TDVStep[]
+    tdvTransitions: TDVTransition[]
+}
